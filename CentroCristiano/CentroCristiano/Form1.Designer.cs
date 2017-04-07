772205e45e6fa4c24294ebaf7942157cf4fdc481
Enmanuel Zabaleta <mybassfbt@outlook.com>
2017-04-07 00:14:56 -0500

    00:14 07/04 Interfaz Principal terminada

--- CentroCristiano/CentroCristiano/Form1.Designer.cs
+++ CentroCristiano/CentroCristiano/Form1.Designer.cs
@@ -29,24 +29,162 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.PanelLogos = new System.Windows.Forms.Panel();
+            this.CCLogo = new System.Windows.Forms.PictureBox();
+            this.Diosbendice = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PastoresLabel = new System.Windows.Forms.Label();
+            this.Pastores = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.RedLabel = new System.Windows.Forms.Label();
+            this.Lider = new System.Windows.Forms.Button();
+            this.LiderLabel = new System.Windows.Forms.Label();
+            this.PanelLogos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CCLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // PanelLogos
+            // 
+            this.PanelLogos.AutoSize = true;
+            this.PanelLogos.BackColor = System.Drawing.SystemColors.Control;
+            this.PanelLogos.Controls.Add(this.CCLogo);
+            this.PanelLogos.Controls.Add(this.Diosbendice);
+            this.PanelLogos.Controls.Add(this.pictureBox1);
+            this.PanelLogos.Location = new System.Drawing.Point(0, 392);
+            this.PanelLogos.MinimumSize = new System.Drawing.Size(1008, 145);
+            this.PanelLogos.Name = "PanelLogos";
+            this.PanelLogos.Size = new System.Drawing.Size(1008, 145);
+            this.PanelLogos.TabIndex = 0;
+            // 
+            // CCLogo
+            // 
+            this.CCLogo.Image = ((System.Drawing.Image)(resources.GetObject("CCLogo.Image")));
+            this.CCLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("CCLogo.InitialImage")));
+            this.CCLogo.Location = new System.Drawing.Point(47, 27);
+            this.CCLogo.Name = "CCLogo";
+            this.CCLogo.Size = new System.Drawing.Size(141, 90);
+            this.CCLogo.TabIndex = 4;
+            this.CCLogo.TabStop = false;
+            // 
+            // Diosbendice
+            // 
+            this.Diosbendice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Diosbendice.Location = new System.Drawing.Point(413, 64);
+            this.Diosbendice.MaximumSize = new System.Drawing.Size(183, 16);
+            this.Diosbendice.MinimumSize = new System.Drawing.Size(183, 16);
+            this.Diosbendice.Name = "Diosbendice";
+            this.Diosbendice.Size = new System.Drawing.Size(183, 16);
+            this.Diosbendice.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(892, 27);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(62, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 90);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PastoresLabel
+            // 
+            this.PastoresLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PastoresLabel.Location = new System.Drawing.Point(387, 105);
+            this.PastoresLabel.Name = "PastoresLabel";
+            this.PastoresLabel.Size = new System.Drawing.Size(185, 45);
+            this.PastoresLabel.TabIndex = 1;
+            this.PastoresLabel.Text = "Pastor";
+            this.PastoresLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Pastores
+            // 
+            this.Pastores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pastores.Image = ((System.Drawing.Image)(resources.GetObject("Pastores.Image")));
+            this.Pastores.Location = new System.Drawing.Point(572, 105);
+            this.Pastores.Name = "Pastores";
+            this.Pastores.Size = new System.Drawing.Size(45, 45);
+            this.Pastores.TabIndex = 2;
+            this.Pastores.UseVisualStyleBackColor = true;
+            // 
+            // Red
+            // 
+            this.Red.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Red.Image = ((System.Drawing.Image)(resources.GetObject("Red.Image")));
+            this.Red.Location = new System.Drawing.Point(572, 166);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(45, 45);
+            this.Red.TabIndex = 4;
+            this.Red.UseVisualStyleBackColor = true;
+            // 
+            // RedLabel
+            // 
+            this.RedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RedLabel.Location = new System.Drawing.Point(387, 166);
+            this.RedLabel.Name = "RedLabel";
+            this.RedLabel.Size = new System.Drawing.Size(185, 45);
+            this.RedLabel.TabIndex = 3;
+            this.RedLabel.Text = "Felipe de Red";
+            this.RedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lider
+            // 
+            this.Lider.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Lider.Image = ((System.Drawing.Image)(resources.GetObject("Lider.Image")));
+            this.Lider.Location = new System.Drawing.Point(572, 227);
+            this.Lider.Name = "Lider";
+            this.Lider.Size = new System.Drawing.Size(45, 45);
+            this.Lider.TabIndex = 6;
+            this.Lider.UseVisualStyleBackColor = true;
+            // 
+            // LiderLabel
+            // 
+            this.LiderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LiderLabel.Location = new System.Drawing.Point(387, 227);
+            this.LiderLabel.Name = "LiderLabel";
+            this.LiderLabel.Size = new System.Drawing.Size(185, 45);
+            this.LiderLabel.TabIndex = 5;
+            this.LiderLabel.Text = "Líder";
+            this.LiderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 537);
+            this.Controls.Add(this.Lider);
+            this.Controls.Add(this.LiderLabel);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.RedLabel);
+            this.Controls.Add(this.Pastores);
+            this.Controls.Add(this.PastoresLabel);
+            this.Controls.Add(this.PanelLogos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1366, 768);
             this.MinimumSize = new System.Drawing.Size(1024, 576);
             this.Name = "Principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Centro Cristiano";
+            this.PanelLogos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CCLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel PanelLogos;
+        private System.Windows.Forms.Label Diosbendice;
+        private System.Windows.Forms.PictureBox CCLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label PastoresLabel;
+        private System.Windows.Forms.Button Pastores;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Label RedLabel;
+        private System.Windows.Forms.Button Lider;
+        private System.Windows.Forms.Label LiderLabel;
     }
 }
 
--- CentroCristiano/CentroCristiano/Form1.cs
+++ CentroCristiano/CentroCristiano/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,33 @@ namespace CentroCristiano
         public Principal()
         {
             InitializeComponent();
+            Color VerdeOscuro = Color.FromArgb(0, 65, 58);
+            Color Verde = Color.FromArgb(14,77,67);
+            Color VerdeClaro = Color.FromArgb(54,115,109);
+            Color Veige = Color.FromArgb(230, 190, 124);
+            Color Blanco = Color.FromArgb(255, 253, 248);
+            BackColor = Blanco;
+            PanelLogos.BackColor = VerdeOscuro;
+            Diosbendice.ForeColor = Veige;
+            Diosbendice.Text = "¡Dios bendice tu " + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(DateTime.Today.ToString("dddd", new CultureInfo("es-ES"))) + "!";
+            PastoresLabel.BackColor = VerdeOscuro;
+            PastoresLabel.ForeColor = Blanco;
+            Pastores.BackColor = VerdeOscuro;
+            Pastores.FlatAppearance.BorderColor = VerdeOscuro;
+            Pastores.FlatAppearance.MouseDownBackColor = VerdeClaro;
+            Pastores.FlatAppearance.MouseOverBackColor = Verde;
+            RedLabel.BackColor = VerdeOscuro;
+            RedLabel.ForeColor = Blanco;
+            Red.BackColor = VerdeOscuro;
+            Red.FlatAppearance.BorderColor = VerdeOscuro;
+            Red.FlatAppearance.MouseDownBackColor = VerdeClaro;
+            Red.FlatAppearance.MouseOverBackColor = Verde;
+            LiderLabel.BackColor = VerdeOscuro;
+            LiderLabel.ForeColor = Blanco;
+            Lider.BackColor = VerdeOscuro;
+            Lider.FlatAppearance.BorderColor = VerdeOscuro;
+            Lider.FlatAppearance.MouseDownBackColor = VerdeClaro;
+            Lider.FlatAppearance.MouseOverBackColor = Verde;
         }
     }
 }
